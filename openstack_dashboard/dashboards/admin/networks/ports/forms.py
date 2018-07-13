7ac52b8e6da3792059f57dfc4e59d556b9aa4b30
liyongqiang <liyongqiang@infinidata.cc>
2018-07-13 10:59:04 +0800

    chang create port

--- openstack_dashboard/dashboards/admin/networks/ports/forms.py
+++ openstack_dashboard/dashboards/admin/networks/ports/forms.py
@@ -26,95 +26,65 @@ from openstack_dashboard import api
 from openstack_dashboard.dashboards.project.networks.ports \
     import forms as project_forms
 
+LOG = logging.getLogger(__name__)
+
 
 LOG = logging.getLogger(__name__)
 VNIC_TYPES = [('normal', _('Normal')), ('direct', _('Direct')),
               ('macvtap', _('MacVTap'))]
 
 
-class CreatePort(forms.SelfHandlingForm):
-    network_name = forms.CharField(label=_("Network Name"),
-                                   widget=forms.TextInput(
-                                       attrs={'readonly': 'readonly'}),
-                                   required=False)
-    network_id = forms.CharField(label=_("Network ID"),
-                                 widget=forms.TextInput(
-                                     attrs={'readonly': 'readonly'}))
-    name = forms.CharField(max_length=255,
-                           label=_("Name"),
-                           required=False)
-    admin_state = forms.ChoiceField(choices=[(True, _('UP')),
-                                             (False, _('DOWN'))],
-                                    label=_("Admin State"))
-    device_id = forms.CharField(max_length=100, label=_("Device ID"),
-                                help_text=_("Device ID attached to the port"),
-                                required=False)
-    device_owner = forms.CharField(max_length=100, label=_("Device Owner"),
-                                   help_text=_("Device owner attached to the "
-                                               "port"),
-                                   required=False)
+class CreatePort(project_forms.CreatePort):
     binding__host_id = forms.CharField(
         label=_("Binding: Host"),
         help_text=_("The ID of the host where the port is allocated. In some "
                     "cases, different implementations can run on different "
                     "hosts."),
         required=False)
-
     failure_url = 'horizon:admin:networks:detail'
 
-    def __init__(self, request, *args, **kwargs):
-        super(CreatePort, self).__init__(request, *args, **kwargs)
-        if api.neutron.is_extension_supported(request, 'binding'):
-            neutron_settings = getattr(settings,
-                                       'OPENSTACK_NEUTRON_NETWORK', {})
-            supported_vnic_types = neutron_settings.get(
-                'supported_vnic_types', ['*'])
-            if supported_vnic_types:
-                if supported_vnic_types == ['*']:
-                    vnic_type_choices = VNIC_TYPES
-                else:
-                    vnic_type_choices = [
-                        vnic_type for vnic_type in VNIC_TYPES
-                        if vnic_type[0] in supported_vnic_types
-                    ]
-
-                self.fields['binding__vnic_type'] = forms.ChoiceField(
-                    choices=vnic_type_choices,
-                    label=_("Binding: VNIC Type"),
-                    help_text=_(
-                        "The VNIC type that is bound to the neutron port"),
-                    required=False)
-
-        if api.neutron.is_extension_supported(request, 'mac-learning'):
-            self.fields['mac_state'] = forms.BooleanField(
-                label=_("MAC Learning State"), initial=False, required=False)
-
     def handle(self, request, data):
+        network_id = self.initial['network_id']
         try:
             # We must specify tenant_id of the network which a subnet is
             # created for if admin user does not belong to the tenant.
-            network = api.neutron.network_get(request, data['network_id'])
-            data['tenant_id'] = network.tenant_id
-            data['admin_state_up'] = (data['admin_state'] == 'True')
-            del data['network_name']
-            del data['admin_state']
-            if 'mac_state' in data:
-                data['mac_learning_enabled'] = data['mac_state']
-                del data['mac_state']
+            network = api.neutron.network_get(request, network_id)
+            params = {
+                'tenant_id': network.tenant_id,
+                'network_id': network_id,
+                'admin_state_up': data['admin_state'],
+                'name': data['name'],
+                'device_id': data['device_id'],
+                'device_owner': data['device_owner'],
+                'binding__host_id': data['binding__host_id']
+            }
 
-            port = api.neutron.port_create(request, **data)
+            if data.get('specify_ip') == 'subnet_id':
+                if data.get('subnet_id'):
+                    params['fixed_ips'] = [{"subnet_id": data['subnet_id']}]
+            elif data.get('specify_ip') == 'fixed_ip':
+                if data.get('fixed_ip'):
+                    params['fixed_ips'] = [{"ip_address": data['fixed_ip']}]
+
+            if data.get('binding__vnic_type'):
+                params['binding__vnic_type'] = data['binding__vnic_type']
+
+            if data.get('mac_state'):
+                params['mac_learning_enabled'] = data['mac_state']
+
+            if 'port_security_enabled' in data:
+                params['port_security_enabled'] = data['port_security_enabled']
+
+            port = api.neutron.port_create(request, **params)
             msg = _('Port %s was successfully created.') % port['id']
-            LOG.debug(msg)
             messages.success(request, msg)
             return port
-        except Exception:
-            msg = _('Failed to create a port for network %s') \
-                % data['network_id']
-            LOG.info(msg)
-            redirect = reverse(self.failure_url,
-                               args=(data['network_id'],))
+        except Exception as e:
+            LOG.info('Failed to create a port for network %(id)s: %(exc)s',
+                     {'id': network_id, 'exc': e})
+            msg = _('Failed to create a port for network %s') % network_id
+            redirect = reverse(self.failure_url, args=(network_id,))
             exceptions.handle(request, msg, redirect=redirect)
-
 
 class UpdatePort(project_forms.UpdatePort):
     # tenant_id = forms.CharField(widget=forms.HiddenInput())
